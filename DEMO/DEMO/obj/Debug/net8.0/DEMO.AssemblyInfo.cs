923e90613636e2ad518ef292c7dadc05fdebca44
CBS-AbhayTomar <abhay.tomar@cogniscient.in>
2025-02-19 15:43:52 +0530

    third commit

--- DEMO/DEMO/obj/Debug/net8.0/DEMO.AssemblyInfo.cs
+++ DEMO/DEMO/obj/Debug/net8.0/DEMO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc514c45042bdc33a32d63f471c8fdcbf6829f49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70eca6614d43b76388285608e4088d55fa889d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
