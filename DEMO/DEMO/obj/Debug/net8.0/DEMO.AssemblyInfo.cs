f2fa679a90494129661b99c658f6f4b6b2084db4
Ayush Nagar <ayush.nagar@cogniscient.in>
2025-02-27 16:54:24 +0530

    merge

--- DEMO/DEMO/obj/Debug/net8.0/DEMO.AssemblyInfo.cs
+++ DEMO/DEMO/obj/Debug/net8.0/DEMO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6d83cba69a4e0e0c61860a80b5a3d480e99d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf05119ab204d9f81468984f65d4e485c059a1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
